6fb5a37a9a8c16afe7c5e1f855a81ea71e871e79
Jon-Ethan Richards <45516368+JE-Richards@users.noreply.github.com>
2024-10-04 11:02:39 +0100

    feat(app): integrate Editor and manage form state
    - Import Editor into App and create an instance of it to be returned for
    display
    - Set up form data structure with useState
    - Define the section titles and  details to be displayed by the editor
    - Implement the handleChanges functions for form management
    - Jest unit tests included and passed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,146 @@
 import './App.css';
+import Editor from './assets/components/editor/Editor.jsx';
+import { useState } from 'react';
 
 function App() {
-  return <></>;
+  const [formData, setFormData] = useState({
+    personalSummary: {
+      firstName: '',
+      lastName: '',
+      profession: '',
+      professionalSummary: '',
+    },
+    contactDetails: {
+      email: '',
+      mobile: '',
+      linkedIn: '',
+      portfolio: '',
+    },
+    experience: [
+      {
+        company: '',
+        role: '',
+        dateFrom: '',
+        dateTo: '',
+        responsibilities: [''],
+      },
+    ],
+    education: [
+      {
+        institute: '',
+        qualification: '',
+        dateFrom: '',
+        dateTo: '',
+      },
+    ],
+    skills: [''],
+    interests: [''],
+  });
+
+  const formDetails = {
+    editor: {
+      title: 'CV Builder',
+      details:
+        'This app lets you create a simple one-page CV. Fill out each of the forms below to populate the CV.',
+    },
+    personalSummary: {
+      title: '1. Personal Details',
+      details:
+        'This critical part of your CV gives potential employers a glimpse of who you are. Think of it as an opportunity to introduce yourself, highlight some of your skills, and provide a brief summary of your career and goals.',
+    },
+    contactDetails: {
+      title: '2. Contact Details',
+      details:
+        "It's important to leave your contact details so employers have multiple ways to get back in touch with you.",
+    },
+    experience: {
+      title: '3. Professional Experience',
+      details:
+        "This is where you can showcase your professional experience and accomplishments to help demonstrate what you could contribute to your potential employer.<br> Don't forget to include a brief description of your duties and accomplishments in the responsibility section. This helps employers understand the scope of your responsibilities and the impact it had.",
+    },
+    education: {
+      title: '4. Education History',
+      details:
+        "Your education history demonstrates your existing qualifications in addition to your commitment to learning and willingess to acquire new skills.<br> If you have multiple qualifications, list them in reverse chronological order with the most recent qualification listed first.<br> If you don't have any formal education, include any relevant certficates or training you may have received.",
+    },
+    skills: {
+      title: '5. Skills',
+      details:
+        "This is an opportunity for you to showcase your job-relevant skills, abilities, and qualifications. Be specific about your skills and match them to the requirements of the job you're applying for.<br> Include both hard and soft skills.",
+    },
+    interests: {
+      title: '6. Hobbies & Interests',
+      details:
+        "Your hobbies and interests can give employers a better sense of who you are and what motivates you.<br> Make sure to list hobbies and interests that are <strong>relevant</strong> to the job you're applying for, and that highlight your personality, skills, and values. For example, if you're applying for a job in tech, you might consider including interests such as AI or cybersecurity.",
+    },
+  };
+
+  const handlePersonalSummaryChanges = (changes) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      personalSummary: {
+        ...prevFormData.personalSummary,
+        ...changes,
+      },
+    }));
+  };
+
+  const handleContactDetailsChanges = (changes) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      contactDetails: {
+        ...prevFormData.contactDetails,
+        ...changes,
+      },
+    }));
+  };
+
+  const handleExperienceChanges = (updatedExperience) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      experience: updatedExperience,
+    }));
+  };
+
+  const handleEducationChanges = (updatedEducation) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      education: updatedEducation,
+    }));
+  };
+
+  const handleSkillsChanges = (updatedSkills) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      skills: updatedSkills,
+    }));
+  };
+
+  const handleInterestsChanges = (updatedInterests) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      interests: updatedInterests,
+    }));
+  };
+
+  const handleChangesFns = {
+    personalSummaryChanges: handlePersonalSummaryChanges,
+    contactDetailsChanges: handleContactDetailsChanges,
+    experienceChanges: handleExperienceChanges,
+    educationChanges: handleEducationChanges,
+    skillsChanges: handleSkillsChanges,
+    interestsChanges: handleInterestsChanges,
+  };
+
+  return (
+    <>
+      <Editor
+        formData={formData}
+        formDetails={formDetails}
+        handleChanges={handleChangesFns}
+      />
+    </>
+  );
 }
 
 export default App;
---
+++ src/tests/App.test.jsx
@@ -0,0 +1,169 @@
+import App from '../App.jsx';
+import '@testing-library/jest-dom';
+import {
+  render,
+  screen,
+  fireEvent,
+  cleanup,
+  within,
+} from '@testing-library/react';
+import { useState } from 'react';
+
+// Mock of Editor component to test App independently of other components
+jest.mock(
+  '../assets/components/editor/Editor.jsx',
+  () =>
+    ({ formData, formDetails, handleChanges }) => {
+      return (
+        <div>
+          <h1>{formDetails.editor.title}</h1>
+
+          {/* buttons to simulate calling the handleChanges functions */}
+          <button
+            onClick={() =>
+              handleChanges.personalSummaryChanges({ firstName: 'Test' })
+            }
+          >
+            Update Personal Summary
+          </button>
+
+          <button
+            onClick={() =>
+              handleChanges.contactDetailsChanges({ email: 'test@test.com' })
+            }
+          >
+            Update Contact Details
+          </button>
+
+          <button
+            onClick={() =>
+              handleChanges.experienceChanges([
+                {
+                  company: 'Test co',
+                  role: '',
+                  dateFrom: '',
+                  dateTo: '',
+                  responsibilities: [''],
+                },
+              ])
+            }
+          >
+            Update Experience
+          </button>
+
+          <button
+            onClick={() =>
+              handleChanges.educationChanges([
+                {
+                  institute: 'Test Uni',
+                  qualification: '',
+                  dateFrom: '',
+                  dateTo: '',
+                },
+              ])
+            }
+          >
+            Update Education
+          </button>
+
+          <button
+            onClick={() => handleChanges.skillsChanges(['testing skill'])}
+          >
+            Update Skills
+          </button>
+
+          <button
+            onClick={() => handleChanges.interestsChanges(['reading tests'])}
+          >
+            Update Interests
+          </button>
+
+          {/* elements to render state data to test changes are retained */}
+          <p>First name: {formData.personalSummary.firstName}</p>
+          <p>Email: {formData.contactDetails.email}</p>
+          <p>Company: {formData.experience[0].company}</p>
+          <p>Institute: {formData.education[0].institute}</p>
+          <p>Skills: {formData.skills[0]}</p>
+          <p>Interests: {formData.interests[0]}</p>
+        </div>
+      );
+    }
+);
+
+beforeEach(() => {
+  jest.clearAllMocks();
+
+  render(<App />);
+});
+
+afterEach(cleanup);
+
+describe('Testing App component', () => {
+  describe('Testing initial render', () => {
+    test('App renders without crashing', () => {
+      expect(screen.getByText(/CV Builder/)).toBeInTheDocument();
+    });
+
+    test('Initial state is set correctly', () => {
+      // Check that the initial values are rendered correctly
+      expect(screen.getByText(/First name:/)).toHaveTextContent('First name:');
+      expect(screen.getByText(/Email:/)).toHaveTextContent('Email:');
+      expect(screen.getByText(/Company:/)).toHaveTextContent('Company:');
+      expect(screen.getByText(/Institute:/)).toHaveTextContent('Institute:');
+      expect(screen.getByText(/Skills:/)).toHaveTextContent('Skills:');
+      expect(screen.getByText(/Interests:/)).toHaveTextContent('Interests:');
+    });
+  });
+
+  describe('Testing state changes', () => {
+    test('handleChanges functions correctly update state values', () => {
+      // Test handlePersonalSummaryChanges
+      const personalSummaryBtn = screen.getByRole('button', {
+        name: /Update Personal Summary/,
+      });
+      const personalSummaryText = screen.getByText(/First name:/);
+      fireEvent.click(personalSummaryBtn);
+      expect(personalSummaryText).toHaveTextContent('First name: Test');
+
+      // Test handleContactDetailsChanges
+      const contactDetailsBtn = screen.getByRole('button', {
+        name: /Update Contact Details/,
+      });
+      const contactDetailsText = screen.getByText(/Email:/);
+      fireEvent.click(contactDetailsBtn);
+      expect(contactDetailsText).toHaveTextContent('Email: test@test.com');
+
+      // Test handleExperienceChanges
+      const experienceBtn = screen.getByRole('button', {
+        name: /Update Experience/,
+      });
+      const experienceText = screen.getByText(/Company:/);
+      fireEvent.click(experienceBtn);
+      expect(experienceText).toHaveTextContent('Company: Test co');
+
+      // Test handleEducationChanges
+      const educationBtn = screen.getByRole('button', {
+        name: /Update Education/,
+      });
+      const educationText = screen.getByText(/Institute:/);
+      fireEvent.click(educationBtn);
+      expect(educationText).toHaveTextContent('Institute: Test Uni');
+
+      // Test handleSkillsChanges
+      const skillsBtn = screen.getByRole('button', {
+        name: /Update Skills/,
+      });
+      const skillsText = screen.getByText(/Skills:/);
+      fireEvent.click(skillsBtn);
+      expect(skillsText).toHaveTextContent('testing skill');
+
+      // Test handleInterestsChanges
+      const interestsBtn = screen.getByRole('button', {
+        name: /Update Interests/,
+      });
+      const interestsText = screen.getByText(/Interests:/);
+      fireEvent.click(interestsBtn);
+      expect(interestsText).toHaveTextContent('Interests: reading tests');
+    });
+  });
+});
